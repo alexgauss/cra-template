50430af3cc48215c8860a15430f1f33441159ffc
Alexander Gauss <alexander.gauss.vienna@gmail.com>
2022-06-15 12:04:14 +0200

    added Application to main entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { App } from "./components/app/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
